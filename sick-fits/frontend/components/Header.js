7a70b47261ec1092b1aea8a30108ad95dc777cb6
Vrymel Omandam <vrymel.dev@gmail.com>
2018-12-24 09:31:05 +0800

    Configure NProgress and styled-component SSR

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
 import Link from 'next/link';
+import Router from 'next/router';
 import styled from 'styled-components';
 import Nav from './Nav';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Logo = styled.h1`
     font-size: 4rem;
