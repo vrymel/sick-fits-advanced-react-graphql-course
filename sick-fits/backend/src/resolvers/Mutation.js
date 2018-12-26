c050dd3c4e654b791cf723578a3d5ac30162ff4b
Vrymel Omandam <vrymel.dev@gmail.com>
2018-12-26 11:46:25 +0800

    Added Item type

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {...args}
+        });
+
+        return item
+    }
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    items(parent, args, ctx, info) {
+        return ctx.db.query.items();
+    }
+};
 
 module.exports = Query;
