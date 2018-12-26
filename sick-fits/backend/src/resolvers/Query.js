20af44585699212c18e360c0b2bc6ed907d2fa69
Vrymel Omandam <vrymel.dev@gmail.com>
2018-12-26 19:44:17 +0800

    Used prisma-binding forwardTo in Query resolver

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,7 +1,7 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    items(parent, args, ctx, info) {
-        return ctx.db.query.items();
-    }
+    items: forwardTo('db'),
 };
 
 module.exports = Query;
