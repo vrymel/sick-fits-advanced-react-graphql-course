b37fa105d667caf96506c81e551dbcf651b64319
Vrymel Omandam <vrymel.dev@gmail.com>
2019-01-01 21:15:56 +0800

    Added User signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -31,6 +34,28 @@ const mutations = {
         const response = await ctx.db.mutation.deleteItem({ where }, info);
 
         return response;
+    },
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+
+        const password = await bcrypt.hash(args.password, 10);
+
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            }
+        }, info);
+
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+
+        return user;
     }
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,7 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
+    itemsConnection: forwardTo('db'),
 };
 
 module.exports = Query;
