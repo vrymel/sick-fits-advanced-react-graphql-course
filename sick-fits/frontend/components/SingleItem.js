7ed5b0edd94d91f4a38d367632ee3a5059f8f6d4
Vrymel Omandam <vrymel.dev@gmail.com>
2019-01-24 10:58:22 +0800

    Added SingleItem snapshot tests

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Error from "./ErrorMessage";
 import styled from "styled-components";
 import Head from "next/head";
-import Error from "./ErrorMessage";
 
 const SingleItemStyles = styled.div`
   max-width: 1200px;
@@ -19,7 +19,7 @@ const SingleItemStyles = styled.div`
     object-fit: contain;
   }
   .details {
-    margin: 2rem;
+    margin: 3rem;
     font-size: 2rem;
   }
 `;
@@ -27,20 +27,27 @@ const SingleItemStyles = styled.div`
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     item(where: { id: $id }) {
+      id
       title
       description
-      price
       largeImage
     }
   }
 `;
-
 class SingleItem extends Component {
   render() {
     return (
-      <Query query={SINGLE_ITEM_QUERY} variables={this.props}>
-        {({ data: { item }, loading, error }) => {
-          if (error) <Error error={error} />;
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id
+        }}
+      >
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
+          const item = data.item;
           return (
             <SingleItemStyles>
               <Head>
@@ -60,3 +67,4 @@ class SingleItem extends Component {
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
